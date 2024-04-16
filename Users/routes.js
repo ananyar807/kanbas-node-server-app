2ed0a3a975d838538661948fae5e241bc6c1a51e
Ananya Radhakrishnan <ananyar807@gmail.com>
2024-04-16 15:42:02 -0400

    changed node env

--- Users/routes.js
+++ Users/routes.js
@@ -56,7 +56,6 @@ export default function UserRoutes(app) {
   };
 
   const profile = async (req, res) => {
-    console.log(req.session["currentUser"]);
     res.json(req.session["currentUser"]);
   };
 
