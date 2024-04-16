9ea949ae309a9b405d136b4cc45246c949e569f0
Ananya Radhakrishnan <ananyar807@gmail.com>
2024-04-16 11:31:11 -0400

    changed domain

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV !== "development") {
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.HTTP_SERVER_DOMAIN,
+    domain: "https://kanbas-node-server-app-dsa7.onrender.com",
   };
 }
 app.use(session(sessionOptions));
